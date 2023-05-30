af5e7eed2924848cb1d723906659413487fb0d1e
Isaac Korankye Asante <xquisit31@gmail.com>
2023-05-30 22:49:59 +0000

    Daily code

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,13 +12,17 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pickk your Category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20
         ),
-        children: [],
+        children: [
+          for (final category in availableCategories)
+          CategoryGridItem(category: category)
+        ],
       ),
     );
   }
