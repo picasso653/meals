aff4d92e16d2c504e5ed7593ec0473ee8bd13f9c
Isaac Korankye Asante <xquisit31@gmail.com>
2023-06-01 19:53:37 +0000

    facing a coouple of problems

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
@@ -10,7 +11,26 @@ class MealItem extends StatelessWidget {
       child: InkWell(
         onTap: () {},
         child: Stack(
-          children: [],
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+            Positioned(
+              right: 0,
+              left: 0,
+              top: 0,
+              bottom: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 45, vertical: 6),
+                color: Colors.black54,
+                child: Column(children: [
+                  Text(meal.title, maxLines: 2,),
+                  const SizedBox(height: 12,),
+
+                ]),
+              ),),
+          ],
         ),
       ),
     );
