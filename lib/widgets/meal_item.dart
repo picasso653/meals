a06408e2502c6090bd40c616a72d0364349b52a5
Isaac Korankye Asante <xquisit31@gmail.com>
2023-06-03 23:47:29 +0000

    Back to code

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
   final Meal meal;
+  String get complexityText {
+    return meal.complexity.name[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
         onTap: () {},
         child: Stack(
@@ -15,22 +26,44 @@ class MealItem extends StatelessWidget {
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
             ),
-            // I've become lazy again
             Positioned(
               right: 0,
               left: 0,
               top: 0,
               bottom: 0,
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 45, vertical: 6),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 45, vertical: 6),
                 color: Colors.black54,
                 child: Column(children: [
-                  Text(meal.title, maxLines: 2,),
-                  const SizedBox(height: 12,),
-
+                  Text(
+                    meal.title,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    children: [
+                      MealItemTrait(
+                          icon: Icons.schedule, label: '${meal.duration} min'),
+                      MealItemTrait(icon: Icons.work, label: meal.id)
+                    ],
+                  )
                 ]),
-              ),),
+              ),
+            ),
           ],
         ),
       ),
