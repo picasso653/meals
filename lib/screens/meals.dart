52fa669ef1f6016a5d7269b8ad4b25a9634f5eea
Isaac Korankye Asante <xquisit31@gmail.com>
2023-06-05 15:16:45 +0000

    now I'm just confused

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
   final String title;
   final List<Meal> meals;
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailScreen(meal: meal),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: ((context, index) => MealItem(meal: meals[index])),
+      itemBuilder: ((context, index) => MealItem(
+            meal: meals[index],
+            onSelectMeal: (Meal meal) {
+              selectMeal(context, meal);
+            },
+          )),
     );
     if (meals.isEmpty) {
       content = Center(
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -4,8 +4,10 @@ import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
+
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
         meal.complexity.name.substring(1);
@@ -26,7 +28,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
@@ -64,11 +68,9 @@ class MealItem extends StatelessWidget {
                     children: [
                       MealItemTrait(
                           icon: Icons.schedule, label: '${meal.duration} min'),
-                      MealItemTrait(icon: Icons.work, 
-                      label: complexityText),
+                      MealItemTrait(icon: Icons.work, label: complexityText),
                       MealItemTrait(
-                          icon: Icons.attach_money, 
-                          label: affordabilityText)
+                          icon: Icons.attach_money, label: affordabilityText)
                     ],
                   )
                 ]),
