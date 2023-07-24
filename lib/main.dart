55bf180d17d9852db25746c0a30a49b8135b34ed
Isaac Korankye Asante <xquisit31@gmail.com>
2023-07-24 21:53:33 +0000

    Back to flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,57 @@
+// Importing necessary packages and dependencies
 import 'package:flutter/material.dart';
 
+// Importing Google Fonts for custom text styles
 import 'package:google_fonts/google_fonts.dart';
+
+// Importing the 'TabsScreen' widget from the 'tabs.dart' file
 import 'package:meals/screens/tabs.dart';
 
+// Defining a custom theme for the app using the 'ThemeData' class
 final theme = ThemeData(
+  // Setting 'useMaterial3' to 'true' enables Material You (Material Design 3.0) theming.
   useMaterial3: true,
+
+  // Defining the color scheme for the app using 'ColorScheme'.
   colorScheme: ColorScheme.fromSeed(
+    // Setting the overall brightness of the app to 'dark'.
     brightness: Brightness.dark,
+    // Defining a seed color that will be used to generate the rest of the colors in the theme.
     seedColor: const Color.fromARGB(255, 131, 57, 0),
   ),
+
+  // Using Google Fonts' 'latoTextTheme()' to set the default text styles for the app.
+  // Google Fonts provides a collection of fonts that can be used in Flutter apps.
   textTheme: GoogleFonts.latoTextTheme(),
 );
 
+// The main entry point of the Flutter app.
 void main() {
+  // Running the app by calling 'runApp()' and passing the 'App' widget as the root of the widget tree.
   runApp(const App());
 }
 
+// Defining the 'App' class, which extends 'StatelessWidget'.
 class App extends StatelessWidget {
+  // Constructor for the 'App' class, using named parameters (super.key).
+  // The '@required' annotation indicates that 'key' must be provided when creating an instance of this class.
+  // However, 'key' is not used explicitly in this code snippet.
   const App({super.key});
 
+  // The 'build' method required by 'StatelessWidget' to describe the user interface.
+  // It returns a 'MaterialApp' widget, which is the root of the app's widget tree.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Setting 'debugShowCheckedModeBanner' to 'false' removes the "Debug" banner
+      // that appears on the top right corner of the app while in debug mode.
       debugShowCheckedModeBanner: false,
+
+      // Applying the custom 'theme' defined earlier to the app.
       theme: theme,
-      home: const TabsScreen()
+
+      // Setting the 'TabsScreen' widget as the home screen of the app.
+      home: const TabsScreen(),
     );
   }
 }
--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 
+// Defining a stateless widget 'MealItemTrait' to represent an item with an icon and a label.
 class MealItemTrait extends StatelessWidget {
-  const MealItemTrait({super.key, required this.icon, required this.label});
+  // Constructor for the 'MealItemTrait' class, using named parameters (super.key, icon, label).
+  // The '@required' annotation indicates that 'icon' and 'label' must be provided when creating an instance of this class.
+  const MealItemTrait({required this.icon, required this.label, Key? key}) : super(key: key);
+
+  // The 'icon' and 'label' properties are declared as final, meaning they cannot be changed after the widget is created.
   final IconData icon;
   final String label;
 
+  // The 'build' method required by 'StatelessWidget' to describe the user interface.
+  // It returns a 'Row' widget containing an 'Icon' widget and a 'Text' widget.
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
+        // Displaying an 'Icon' widget with the provided 'icon' data.
         Icon(
           icon,
           size: 17,
           color: Colors.white,
         ),
+        // Adding a horizontal spacing of 6 pixels using 'SizedBox'.
         const SizedBox(
           width: 6,
         ),
+        // Displaying a 'Text' widget with the provided 'label' data.
         Text(
           label,
           style: const TextStyle(color: Colors.white),
