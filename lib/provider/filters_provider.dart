a22c751b45847feae291dc26ce4f5c8c829a5835
Isaac Korankye Asante <xquisit31@gmail.com>
2023-12-19 21:31:44 +0000

    Connectin multiple providers with each other

--- lib/provider/filters_provider.dart
+++ lib/provider/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/provider/meals_provider.dart';
 
 enum Filter { glutenFreee, lactoseFree, vegetarian, vegan }
 
@@ -23,3 +24,24 @@ final filtersProvider =
 StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
   (ref) => FiltersNotifier(),
 );
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+  return meals.where((meal) {
+      if (activeFilters[Filter.glutenFreee]! && !meal.isGlutenFree) {
+        return false;
+      }
+
+      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
+});
